0da760fb236af1df608cc4db9c77ca6bcc86547e
刘逢朔 <fsliu136@gmail.com>
2025-03-12 17:44:05 +0800

    feat:修改测试入口

--- FSAVDemo/ViewController/FSAVDemoVC.swift
+++ FSAVDemo/ViewController/FSAVDemoVC.swift
@@ -7,110 +7,105 @@
 
 import UIKit
 
-class FSAVDemoVC : FSAVBaseVC {
-    
+class FSAVDemoVC: FSAVBaseVC, UITableViewDelegate, UITableViewDataSource {
+
     deinit {
         
     }
-    
-    lazy var avButton: UIButton = {
-        let button: UIButton = UIButton(type: .custom)
-        button.frame = CGRect(x: 100, y: 80, width: 180, height: 50)
-        button.setTitle("AVFoundation Demo", for: .normal)
-        button.backgroundColor = .green
-        button.titleLabel?.textColor = .white
-        
-        button.layer.masksToBounds = true
-        button.layer.cornerRadius = 8
-        
-        button.addTarget(self, action: #selector(gotoAVFoundation(sender:)), for: .touchUpInside)
-        
-        return button;
+
+    // 列表数据
+    let demoOptions = [
+        "音频相关": [
+            ("AVFoundation Demo", #selector(gotoAVFoundation)),
+            ("FFmpeg Demo", #selector(gotoFFmpeg))
+        ],
+        "图形相关": [
+            ("OpenGL Demo", #selector(gotoOpenGL)),
+            ("Video Play Demo", #selector(gotoVideoPlay))
+        ],
+        "智能设备相关": [
+            ("PassKit Demo", #selector(gotoPassKit))
+        ]
+    ]
+
+    // 创建 UITableView
+    lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: self.view.bounds, style: .grouped)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
     }()
-    
-    lazy var ffmpegButton: UIButton = {
-        let button: UIButton = UIButton(type: .custom)
-        button.frame = CGRect(x: 100, y: 160, width: 180, height: 50)
-        button.setTitle("FFmpeg Demo", for: .normal)
-        button.backgroundColor = .green
-        button.titleLabel?.textColor = .white
-        
-        button.layer.masksToBounds = true
-        button.layer.cornerRadius = 8
-        
-        button.addTarget(self, action: #selector(gotoFFmpeg(sender:)), for: .touchUpInside)
-        
-        return button;
-    }()
-    
-    lazy var openGLButton: UIButton = {
-        let button: UIButton = UIButton(type: .custom)
-        button.frame = CGRect(x: 100, y: 240, width: 180, height: 50)
-        button.setTitle("OpenGL Demo", for: .normal)
-        button.backgroundColor = .green
-        button.titleLabel?.textColor = .white
-        
-        button.layer.masksToBounds = true
-        button.layer.cornerRadius = 8
-        
-        button.addTarget(self, action: #selector(gotoOpenGL(sender:)), for: .touchUpInside)
-        
-        return button;
-    }()
-    
-    
-    lazy var videoPlayButton: UIButton = {
-        let button: UIButton = UIButton(type: .custom)
-        button.frame = CGRect(x: 100, y: 320, width: 180, height: 50)
-        button.setTitle("video play Demo", for: .normal)
-        button.backgroundColor = .green
-        button.titleLabel?.textColor = .white
-        
-        button.layer.masksToBounds = true
-        button.layer.cornerRadius = 8
-        
-        button.addTarget(self, action: #selector(gotoVideoPlay(sender:)), for: .touchUpInside)
-        
-        return button;
-    }()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        title = "音视频示例"
+        title = "示例"
         
         setupUI()
     }
-    
+
     private func setupUI() {
-        
-        view.addSubview(avButton)
-        view.addSubview(ffmpegButton)
-        view.addSubview(openGLButton)
-        view.addSubview(videoPlayButton)
+        view.addSubview(tableView)
     }
-    
-    @objc private func gotoAVFoundation(sender: UIButton) {
+
+    // UITableViewDataSource 方法
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return demoOptions.keys.count
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let key = Array(demoOptions.keys)[section]
+        return demoOptions[key]?.count ?? 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let key = Array(demoOptions.keys)[indexPath.section]
+        cell.textLabel?.text = demoOptions[key]?[indexPath.row].0
+        return cell
+    }
+
+    // UITableViewDelegate 方法
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return Array(demoOptions.keys)[section]
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let key = Array(demoOptions.keys)[indexPath.section]
+        let selector = demoOptions[key]?[indexPath.row].1
+        perform(selector!)
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+    @objc private func gotoAVFoundation() {
         let vc = FSAVFoundationVC()
         vc.title = "AVFoundation Demo"
         navigationController?.pushViewController(vc, animated: true)
     }
     
-    @objc private func gotoFFmpeg(sender: UIButton) {
+    @objc private func gotoFFmpeg() {
         let vc = FSFFmpegVC()
         vc.title = "FFmpeg Demo"
         navigationController?.pushViewController(vc, animated: true)
     }
     
-    @objc private func gotoOpenGL(sender: UIButton) {
+    @objc private func gotoOpenGL() {
         let vc = FSOpenGLVC()
         vc.title = "OpenGL Demo"
         navigationController?.pushViewController(vc, animated: true)
     }
     
-    @objc private func gotoVideoPlay(sender: UIButton) {
+    @objc private func gotoVideoPlay() {
         let path = Bundle.main.path(forResource: "test.flv", ofType: nil) ?? ""
-        let vc = FSVideoPlayDemoVC.viewController(withContentPath:path, contentFrame:self.view.bounds, parameters: [:])
+        let vc = FSVideoPlayDemoVC.viewController(withContentPath: path, contentFrame: self.view.bounds, parameters: [:])
         navigationController?.pushViewController(vc as! UIViewController, animated: true)
     }
+    
+    @objc private func gotoPassKit() {
+        let vc = FSPassKitVC()
+        vc.title = "PassKit Demo"
+        navigationController?.pushViewController(vc, animated: true)
+    }
 }
+
---
+++ FSAVDemo/ViewController/FSPassKitVC.swift
@@ -0,0 +1,12 @@
+//
+//  FSPassKitVC.swift
+//  FSAVDemo
+//
+//  Created by liufengshuo on 2025/3/7.
+//
+
+import Foundation
+
+class FSPassKitVC: FSAVBaseVC {
+    
+}
